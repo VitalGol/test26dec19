04833673f4b1ab8f8870494dece4a5a903c1118e
Vitaly <vitalgolob@gmail.com>
2020-01-24 14:32:39 +0200

    Internationalization

---
+++ App.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react';
+import {StyleSheet, View, Text, Button} from 'react-native';
+import Internationalization from './src/screens/Internationalization';
+
+const App = () => <Internationalization />;
+
+export default App;
---
+++ src/screens/Internationalization.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import {StyleSheet, Text, View, Button} from 'react-native';
+import I18n from 'react-native-i18n';
+
+export default function App() {
+  return (
+    <View>
+      <Text>'Welcome to React'</Text>
+      <Text>{I18n.t('greeting')}</Text>
+      <Text>{I18n.t('goodbye')}</Text>
+    </View>
+  );
+}
+
+I18n.fallbacks = true;
+I18n.translations = {
+  en: {
+    greeting: 'Hello',
+    goodbye: 'Bye',
+  },
+  fr: {
+    greeting: 'Bonjour',
+    goodbye: 'Au Revoir',
+  },
+  es: {
+    greeting: 'Hola',
+    goodbye: 'Adios',
+  },
+};
